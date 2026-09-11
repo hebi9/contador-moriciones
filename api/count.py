c663dcd9ff1da50f1e034d1f1babc37c5a575681
hebi9 <faoa.16@gmail.com>
2026-09-10 22:32:22 -0600

    Arreglar rutas Flask para Vercel

--- api/count.py
+++ api/count.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 
 contador = {"count": 179}
 
-@app.route("/api/count", methods=["GET"])
+@app.route("/", methods=["GET"])
 def get_count():
     return jsonify(contador)
 
-@app.route("/api/count", methods=["POST"])
+@app.route("/", methods=["POST"])
 def set_count():
-    data = request.json
+    data = request.get_json()
     contador["count"] = int(data["count"])
-    return jsonify(ok=True, count=contador["count"])
+    return jsonify({"ok": True, "count": contador["count"]})
