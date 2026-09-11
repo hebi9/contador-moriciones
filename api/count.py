7956b34c1d10b890cdd33468dd1307b8edb9e8c2
hebi9 <faoa.16@gmail.com>
2026-09-10 22:39:03 -0600

    Servir index desde Flask

--- api/count.py
+++ api/count.py
@@ -1,14 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+from pathlib import Path
 
 app = Flask(__name__)
 
 contador = {"count": 179}
 
-@app.route("/", methods=["GET"])
+ROOT = Path(__file__).resolve().parent.parent
+
+# Página principal
+@app.route("/")
+def index():
+    return send_from_directory(ROOT, "index.html")
+
+# API
+@app.route("/api/count", methods=["GET"])
 def get_count():
     return jsonify(contador)
 
-@app.route("/", methods=["POST"])
+@app.route("/api/count", methods=["POST"])
 def set_count():
     data = request.get_json()
     contador["count"] = int(data["count"])
